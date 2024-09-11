cfd2406a79803f9454de0b03086653cd3f60a81a
Muhammed Badhusha K <41858648+wazimBadsha@users.noreply.github.com>
2024-09-11 19:14:13 +0530

    added fix for signed url upload

--- controllers/fileUploadController.js
+++ controllers/fileUploadController.js
@@ -1,73 +1,58 @@
+
+
 require("dotenv").config();
 const AWS = require("aws-sdk");
-// to get content type of file
-var mime = require('mime-types');
-// to generate md5 hash for file key
 const md5 = require("md5");
 const path = require("path");
-const _ = require("lodash");
-
+var mime = require('mime-types');
 const apiResponse = require("../helpers/apiResponse");
 
-AWS.config = new AWS.Config();
-AWS.config.accessKeyId = process.env.AWS_SECRET_S3;
-AWS.config.secretAccessKey = process.env.AWS_SECRET_ACCESS_S3; 
-!_.isEmpty(process.env.AWS_REGION_S3) && (AWS.config.region = process.env.AWS_REGION_S3);
+AWS.config = new AWS.Config({
+  accessKeyId: process.env.AWS_SECRET_S3,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_S3,
+  region: process.env.AWS_REGION_S3,
+  signatureVersion: 'v4'
+});
 
-exports.getSignedUrl = [
-  async function (req, res) {
-    try {
-      const { filePath = null } = req.body;
+const s3 = new AWS.S3();
 
-      // Uses node-mime to detect mime-type based on file extension
-      const mimeType = mime.contentType(filePath);
+exports.getSignedUrl = async function (req, res) {
+  try {
+    const { filePath = null } = req.body;
+    const mimeType = mime.contentType(filePath);
+    const signedUrlExpireSeconds = process.env.S3_SIGNED_URL_EXPIRY
+      ? parseInt(process.env.S3_SIGNED_URL_EXPIRY)
+      : 10000;
 
-      // Time after which this signed url will expire
-      const signedUrlExpireSeconds = 75000
-        ? parseInt(process.env.S3_SIGNED_URL_EXPIRY)
-        : 10000;
+    const md5Hash = md5(new Date().getTime());
+    const fileExtension = path.extname(filePath);
+    let fileKey = `${md5Hash}${fileExtension}`;
+    let uploadFolder = process.env.AWS_FOLDER_S3;
+    const myBucket = process.env.AWS_S3_BUCKET_NAME;
+    const s3ObjectKey = `${uploadFolder}/${fileKey}`;
 
-      // create filename
-      const md5Hash = md5(new Date().getTime());
+    const params = {
+      Bucket: myBucket,
+      Key: s3ObjectKey,
+      Expires: signedUrlExpireSeconds,
+      ContentType: mimeType,
+      ACL: 'public-read'  // <-- Add this to make the file publicly readable
+    };
 
-      const fileExtension = path.extname(filePath);
+    const signedUrl = await s3.getSignedUrlPromise('putObject', params);
+    
+    apiResponse.successResponseWithData(res, "Signed URL", {
+      status: "success",
+      data: {
+        signedUrl,
+        s3ObjectKey,
+        downloadUrl: `https://${process.env.AWS_S3_BUCKET_NAME}.s3.${process.env.AWS_REGION_S3}.amazonaws.com/${s3ObjectKey}`,
+        mimeType,
+      },
+    });
 
-      let fileKey = `${md5Hash}${fileExtension}`;
-
-      let uploadFolder = process.env.AWS_FOLDER_S3;
-
-      const s3 = new AWS.S3();
-
-      const myBucket = process.env.AWS_S3_BUCKET_NAME;
-      const s3ObjectKey = `${uploadFolder}/${fileKey}`;
-
-      const params = {
-        Bucket: myBucket,
-        Key: s3ObjectKey,
-        Expires: signedUrlExpireSeconds,
-        ContentType: mimeType,
-      };
-
-      s3.getSignedUrl("putObject", params, function (err, url) {
-        if (err) {
-          console.log("[FileUploadController-getSignedUrl]-Error getting presigned url from AWS S3", err);
-          return apiResponse.ErrorResponse(res, err);
-        } else {
-          apiResponse.successResponseWithData(res, "Signed URL", {
-            status: "success",
-            data: {
-              signedUrl: url,
-              s3ObjectKey,
-              downloadUrl: `https://${process.env.AWS_S3_BUCKET_NAME}.s3.${process.env.AWS_REGION_S3}.amazonaws.com/${s3ObjectKey}`,
-              mimeType,
-            },
-          });
-        }
-      });
-    } catch (err) {
-      console.log("[FileUploadController-getSignedUrl]-Error", err);
-      //throw error in json response with status 500.
-      return apiResponse.ErrorResponse(res, err);
-    }
-  },
-];
+  } catch (err) {
+    console.error("[FileUploadController-getSignedUrl]-Error", err);
+    return apiResponse.ErrorResponse(res, err);
+  }
+};
